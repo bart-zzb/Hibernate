5493e41c2c6f92ed12678285f1b7d13967cb6267
bart_zzb <bartzhaozb@gmail.com>
2024-06-18 22:53:32 +0800

    2024-06-18 Hibernate 查询使用SQLQuery对象

--- hibernate_module2/src/test/java/Test/HibernateAPITest.java
+++ hibernate_module2/src/test/java/Test/HibernateAPITest.java
@@ -5,10 +5,12 @@ import org.example.utils.HibernateUtils;
 import org.hibernate.*;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
+//查询三种方法
 public class HibernateAPITest {
-    //使用query对象
+    //1.使用query对象
     @Test
     public void testQuery(){
         SessionFactory sessionFactory = null;
@@ -37,7 +39,7 @@ public class HibernateAPITest {
         }
     }
 
-    //使用Criteria对象
+    //2.使用Criteria对象
     @Test
     public void testCriteria(){
         SessionFactory sessionFactory = null;
@@ -51,6 +53,42 @@ public class HibernateAPITest {
             //创建Criteria对象
             Criteria criteria = session.createCriteria(User.class);
             List<User> list = criteria.list();
+            for (User user : list) {
+                System.out.println(user);
+            }
+
+            tx.commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+//            session.close();
+            sessionFactory.close();
+        }
+    }
+
+    //3.使用SQLQuery对象
+    @Test
+    public void testSQLQuery(){
+        SessionFactory sessionFactory = null;
+        Session session = null;
+        Transaction tx = null;
+        try{
+            sessionFactory = HibernateUtils.getSessionFactory();
+            session = HibernateUtils.getSession();
+            tx = session.beginTransaction();
+
+            //创建SQLQuery对象
+            //参数是普通的sql语句
+            SQLQuery sqlQuery = session.createSQLQuery("select * from t_user");
+            //返回list集合，默认里面每部分数组结构
+//            List<Object[]> list = sqlQuery.list();
+//            for (Object[] objects : list) {
+//                System.out.println(Arrays.toString(objects));
+//            }
+
+            //返回的list中每部分是对象形式
+            sqlQuery.addEntity(User.class);
+            List<User> list = sqlQuery.list();
             for (User user : list) {
                 System.out.println(user);
             }
