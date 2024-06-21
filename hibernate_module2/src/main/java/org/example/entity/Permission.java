30a663214b71a7ff061ec3869846620bdbcf329b
bart_zzb <bartzhaozb@gmail.com>
2024-06-21 16:47:44 +0800

    2024-06-21 Hibernate 多对多级联维护第三张表

--- hibernate_module2/src/main/java/org/example/entity/Permission.java
+++ hibernate_module2/src/main/java/org/example/entity/Permission.java
@@ -2,12 +2,15 @@ package org.example.entity;
 
 import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Permission {
     Long id;
--- hibernate_module2/src/test/java/Test/HibernateManyToMany.java
+++ hibernate_module2/src/test/java/Test/HibernateManyToMany.java
@@ -9,6 +9,40 @@ import org.hibernate.Transaction;
 import org.junit.Test;
 
 public class HibernateManyToMany {
+    //演示多对多维护第三张表
+    @Test
+    public void testTable1() {
+        SessionFactory sessionFactory = null;
+        Session session = null;
+        Transaction tx = null;
+        try {
+            //得到sessionFactory
+            sessionFactory = HibernateUtils.getSessionFactory();
+            //得到session
+            session = sessionFactory.openSession();
+            //开启事务
+            tx = session.beginTransaction();
+
+            Role role = session.get(Role.class, 1L);
+            Permission permission = session.get(Permission.class, 2L);
+            //让id为1的role添加id为2的权限
+//            role.getPermissions().add(permission);
+
+            //让id为1的role删除id为2的权限
+            role.getPermissions().remove(permission);
+            //提交事务
+            tx.commit();
+
+        }catch(Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        }finally {
+            session.close();
+            //sessionFactory不需要关闭
+            sessionFactory.close();
+        }
+    }
+
     //演示多对多级联保存
     @Test
     public void testSave() {
