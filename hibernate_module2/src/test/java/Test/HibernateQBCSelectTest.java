a07db12b967c96a1a27130b8dc08d23607de195e
bart_zzb <bartzhaozb@gmail.com>
2024-06-23 21:34:59 +0800

    2024-06-23 Hibernate QBC条件查询和模糊查询

--- hibernate_module2/src/test/java/Test/HibernateQBCSelectTest.java
+++ hibernate_module2/src/test/java/Test/HibernateQBCSelectTest.java
@@ -7,6 +7,7 @@ import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 import org.junit.Test;
 
 import java.util.List;
@@ -26,6 +27,43 @@ public class HibernateQBCSelectTest {
             //1 创建对象
             Criteria criteria = session.createCriteria(Customer.class);
             //2 调用方法得到结果
+            List<Customer> list = criteria.list();
+            for (Customer customer : list) {
+                System.out.println(customer.getCid()+"::"+customer.getCustName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            session.close();
+            sessionFactory.close();
+        }
+    }
+
+    //QBC条件查询
+    @Test
+    public void testSelect2() {
+        SessionFactory sessionFactory = null;
+        Session session = null;
+        Transaction tx = null;
+        try {
+            sessionFactory = HibernateUtils.getSessionFactory();
+            session = sessionFactory.openSession();
+            tx = session.beginTransaction();
+
+            //1 创建对象
+            Criteria criteria = session.createCriteria(Customer.class);
+
+            //2 首先使用add方法, 表示设置条件值
+            //类似于cid=?
+//            criteria.add(Restrictions.eq("cid",2));
+//            criteria.add(Restrictions.eq("custName", "google"));
+
+            criteria.add(Restrictions.like("custName","go%"));
+
+            //3 调用方法得到结果
             List<Customer> list = criteria.list();
             for (Customer customer : list) {
                 System.out.println(customer.getCid()+"::"+customer.getCustName());
