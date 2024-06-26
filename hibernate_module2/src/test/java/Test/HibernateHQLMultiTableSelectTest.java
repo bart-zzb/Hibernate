cae4fc4370da3e2bab2976bf6e96a4c14627ac5a
bart_zzb <bartzhaozb@gmail.com>
2024-06-26 22:20:33 +0800

    2024-06-26 Hibernate HQL左外连接/迫切左外连接

--- hibernate_module2/src/test/java/Test/HibernateHQLMultiTableSelectTest.java
+++ hibernate_module2/src/test/java/Test/HibernateHQLMultiTableSelectTest.java
@@ -57,7 +57,65 @@ public class HibernateHQLMultiTableSelectTest {
             List<Customer> list = query.list();
 
             for (Customer object : list) {
+                System.out.println(object.getCid()+object.getCustName()+object.getCustPhone()+object.getSetLinkMan());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            session.close();
+            sessionFactory.close();
+        }
+    }
+
+    //HQL左外连接查询, 返回的是数组集合
+    @Test
+    public void testSelect3() {
+        SessionFactory sessionFactory = null;
+        Session session = null;
+        Transaction tx = null;
+        try {
+            sessionFactory = HibernateUtils.getSessionFactory();
+            session = sessionFactory.openSession();
+            tx = session.beginTransaction();
+
+            //1 创建query对象
+            Query query = session.createQuery("from Customer c left outer join c.setLinkMan");
+            List list = query.list();
+
+            for (Object object : list) {
                 System.out.println(object);
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            session.close();
+            sessionFactory.close();
+        }
+    }
+
+    //HQL迫切左外连接查询, 返回的是对象集合
+    @Test
+    public void testSelect4() {
+        SessionFactory sessionFactory = null;
+        Session session = null;
+        Transaction tx = null;
+        try {
+            sessionFactory = HibernateUtils.getSessionFactory();
+            session = sessionFactory.openSession();
+            tx = session.beginTransaction();
+
+            //1 创建query对象
+            Query query = session.createQuery("from Customer c left outer join fetch c.setLinkMan");
+            List<Customer> list = query.list();
+
+            for (Customer object : list) {
+                System.out.println(object.getCid()+object.getCustName()+object.getCustPhone()+object.getSetLinkMan());
             }
 
             tx.commit();
